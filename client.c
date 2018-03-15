ba0e9ae925c8a02d9171bb8b8417d7c0a604ad7d
Andrew Xue <andyzhouyangxue@g.ucla.edu>
2018-03-15 15:36:26 -0700

    client

--- client.c
+++ client.c
@@ -21,6 +21,7 @@ const int MAX_PACKET_SIZE = 1024;
 const int HEADER_SIZE = 32;
 const int MAX_PAYLOAD_SIZE = 992;
 const int MAX_RETRANS_TIME = 500;
+const int WSIZE = 5;
 
 int last_seq_num = 0;
 
@@ -39,7 +40,7 @@ int check_time_out(int sock_fd){
 
 }
 
-struct packet_info
+struct packet
 {
     //Packet Types:
     //SYN and SYN ACK: 0
@@ -50,7 +51,7 @@ struct packet_info
     
     int type;
     int seq_num;
-    int max_no;
+    int max_seq_num;
     int fin;
     int error;
     double time;
@@ -95,7 +96,7 @@ int main(int argc, char *argv[]) {
     //connection successful 
     //client sends the first ack message to server 
     
-    struct packet_info response_packet;
+    struct packet response_packet;
     memset((char *) &response_packet, 0, sizeof(response_packet));
     
     
@@ -162,17 +163,15 @@ int main(int argc, char *argv[]) {
     //send to the server with name 
     char newFilename[MAX_PACKET_SIZE];
     
-    int window_size = 5;
     
-    struct packet_info *buffer;
-    buffer = (struct packet_info *) malloc(window_size * sizeof(struct packet_info));
-    int i;
-    for (i = 0; i<window_size; i++)
-        memset(&(buffer[i]), -1, sizeof(struct packet_info));
+    struct packet *window;
+    window = (struct packet *) malloc(WSIZE * sizeof(struct packet));
+    int x;
+    for (x = 0; x < WSIZE; x++)
+        memset(&(window[x]), 0, sizeof(struct packet));
     
-    int start_of_seq = 0;
-    int end_of_seq = window_size;
-    char* status = "";
+    int window_start = 0;
+    
     while(1)
     {
         recvfrom(sockfd, &response_packet, sizeof(response_packet), 0, (struct sockaddr *) &serv_addr, &(serv_len));
@@ -181,60 +180,60 @@ int main(int argc, char *argv[]) {
         if (response_packet.error == 1) {
             printf("Error 404: File Not Found!\n");
             return 0;
-        }
-                
+        }     
+        char* retrans = "";
         if (response_packet.type == 3)
-            status = "Retransmission";
-
+            retrans = "Retransmission";
 
         fprintf(stdout, "Receiving packet %d\n", response_packet.seq_num);
         
+
+        int window_end = WSIZE;
+        if (window_end > response_packet.max_seq_num)
+            window_end = response_packet.max_seq_num;
         
-        if (end_of_seq > response_packet.max_no)
-            end_of_seq = response_packet.max_no;
-        else
-            end_of_seq = window_size;
-        
-        
-        int current_pkt = (response_packet.seq_num + response_packet.seq_count*30720) / MAX_PAYLOAD_SIZE;
+        int window_curr = (response_packet.seq_num + response_packet.seq_count*30720) / MAX_PAYLOAD_SIZE;
+
+
         // 1. pkt # in [rcvbase, rcvbase+N-1]
-        if ((current_pkt - start_of_seq) >= 0
-            && (current_pkt - start_of_seq) < window_size){
+        int front = window_curr - window_start;
+        if ((front >= 0) && (front < WSIZE)){
             // send ACK
-            struct packet_info ack_packet;
+            struct packet ack_packet;
             memset((char *) &ack_packet, 0, sizeof(ack_packet));
+
             ack_packet.type = 2;
             ack_packet.seq_num = response_packet.seq_num;
             ack_packet.seq_count = response_packet.seq_count;
+
             sendto(sockfd, &ack_packet, sizeof(ack_packet), 0, (struct sockaddr *) &serv_addr, serv_len);
-            printf("Sending packet %d %s\n", ack_packet.seq_num, status);
+            printf("Sending packet %d %s\n", ack_packet.seq_num, retrans);
             
-            // buffer first
-            memcpy(&(buffer[current_pkt - start_of_seq]), &response_packet, sizeof(struct packet_info));
+            memcpy(&(window[front]), &response_packet, sizeof(struct packet));
             
             while (1){
-                
-                if ((buffer[0].seq_num+buffer[0].seq_count*30720) / MAX_PAYLOAD_SIZE == start_of_seq) {
-                    fwrite(buffer[0].data, sizeof(char), buffer[0].data_size, fp);
+
+                int first_packet_in_window = (window[0].seq_num+window[0].seq_count*30720) / MAX_PAYLOAD_SIZE;
+                if (first_packet_in_window == window_start) {
+                    fwrite(window[0].data, sizeof(char), window[0].data_size, fp);
                     
-                    for (i = 0; i<window_size-1; i++){
-                        //shift the buffer to the left
-                        memcpy(&(buffer[i]), &(buffer[i+1]), sizeof(struct packet_info));
+                    // update window
+                    int y = 0;
+                    for (y = 0; y < WSIZE-1; y++){
+                        memcpy(&(window[y]), &(window[y+1]), sizeof(struct packet));
                     }
-                    memset(&(buffer[window_size-1]), -1, sizeof(struct packet_info));
-                    start_of_seq++;
+                    //memset(&(window[WSIZE-1]), 0, sizeof(struct packet));
+                    window_start++;
                 }
                 else
                     break;
             }
         }
         
-        
-        memset((char *) &response_packet.data, 0, sizeof(response_packet.data));
+        //memset((char *) &response_packet.data, 0, sizeof(response_packet.data));
         
         if (response_packet.fin == 1) {
-            printf("Transmission done.\nClosing connection...\n");
-            struct packet_info fin_packet;
+            struct packet fin_packet;
             memset((char *) &fin_packet, 0, sizeof(fin_packet));
             fin_packet.fin = 2;
             fin_packet.seq_num = response_packet.seq_num;
@@ -246,19 +245,19 @@ int main(int argc, char *argv[]) {
                 printf("Sending packet %d FIN\n", response_packet.seq_num);
                 
                 int check_time = check_time_out(sockfd);
-                
-                // if timeout and no acks were received, break and maintain window position
+
                 if(check_time < 1)
                 {
                     attempt++;
                     if (attempt == 3) {
                         printf("Transmission finished.\nConnection closed.\n");
-                        return 0;
+                        close(sockfd);
+                        exit(0);
                     }
                     continue;
                 }
                 // otherwise, fetch the ack
-                struct packet_info ack;
+                struct packet ack;
                 recvfrom(sockfd, &ack, sizeof(ack), 0, (struct sockaddr *) &serv_addr, &(serv_len));
                 
                 if (ack.fin == 2) {
